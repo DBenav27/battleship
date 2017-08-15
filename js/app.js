213d4c2f009ec693bd4ce7b67e28841d4539160b
Daniel Benavidez <danielbenavidez27@gmail.com>
2017-08-15 14:22:51 -0500

    rendered board

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var model {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipSunk: 0,
+
+  ships: [
+    { loctions: [0,0,0], hits: ["","","",]},
+    { loctions: [0,0,0], hits: ["","","",]},
+    { loctions: [0,0,0], hits: ["","","",]}
+  ],
+
+  fire: function(guess) {
+    for var( i=0; i < this.numShips; i ++) {
+
+    }
+  }
+}
