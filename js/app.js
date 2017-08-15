47292cf328448d87bcb2fa14eb344711a4e58e82
Daniel Benavidez <danielbenavidez27@gmail.com>
2017-08-15 15:14:29 -0500

    added fire functionality

--- js/app.js
+++ js/app.js
@@ -1,18 +1,28 @@
-var model {
-  boardSize: 7,
-  numShips: 3,
-  shipLength: 3,
-  shipSunk: 0,
+var model = {
+      boardSize: 7,
+      numShips: 3,
+      shipLength: 3,
+      shipSunk: 0,
 
-  ships: [
-    { loctions: [0,0,0], hits: ["","","",]},
-    { loctions: [0,0,0], hits: ["","","",]},
-    { loctions: [0,0,0], hits: ["","","",]}
-  ],
+      ships: [
+        { locations: [0,0,0], hits: ["","","",]},
+        { locations: [0,0,0], hits: ["","","",]},
+        { locations: [0,0,0], hits: ["","","",]}
+      ],
 
-  fire: function(guess) {
-    for var( i=0; i < this.numShips; i ++) {
+      fire: function(guess) {
+        for (var i=0; i < this.numShips; i++) {
+              var ship = this.ships[i];
+              var index = ship.locations.indexOf(guess);
 
-    }
-  }
+              if (ship.hits[index] === "hit") {
+                  view.displayMessage("you have already hit");
+                  return true;
+              }
+              else if (index >= 0) {
+                  ship.hits(index)
+              }
+        }
+
+      }
 }
