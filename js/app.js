87abfb28fd086e4acf64383ff22c4ce4d7e8aff8
Daniel Benavidez <danielbenavidez27@gmail.com>
2017-08-15 18:34:11 -0500

    fixed errors

--- js/app.js
+++ js/app.js
@@ -46,7 +46,7 @@ var model = {
       },
 
     generateShipLocations: function() {
-        var locations:
+        var locations;
 
         for (var i=0; i < this.numShips; i++) {
             do {
@@ -72,7 +72,7 @@ var model = {
     }
 
     var newShipLocations = [];
-    for (var i=0, i < this.shipLength; i++) {
+    for (var i=0; i < this.shipLength; i++){
         if (direction === 1){
             newShipLocations.push(row + "" + (col + i));
         } else {
@@ -116,7 +116,7 @@ var view = {
   };
 
   var controller = {
-      guesses: 0;
+      guesses: 0,
 
       processGuess: function(guess) {
         var location = parseGuess(guess);
@@ -143,7 +143,7 @@ var view = {
 
           if (isNaN(row) || isNaN(column)) {
               alert("Not a valid input");
-          } else if (row < 0) || row >= model.boardSize || column < 0 || column >=
+          } else if (row < 0 || row >= model.boardSize || column < 0 || column >=
                      model.boardSize) {
               alert("Input is not located on this board");
             } else {
@@ -162,3 +162,27 @@ var view = {
      guess.value = "";
 
   };
+
+  function handleKeyPress(e) {
+    var fireButton = document.getElementbyId("fireButton");
+
+    e=e || window.event;
+
+    if (e.keycode === 13){
+      fireButton.click();
+      return false;
+    }
+  };
+
+  function init() {
+      var fireButton = document.getElementById("fireButton");
+      fireButton.onclick = handleFireButton;
+
+      var guessInput = document.getElementById("guessInput");
+      guessInput.onkeypress = handleKeyPress;
+
+      model.generateShipLocations();
+
+  };
+
+  window.onload = init;
