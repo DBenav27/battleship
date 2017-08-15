a45cfd099b5c0d4825e7a2cd66ed63d7f8c639a6
Daniel Benavidez <danielbenavidez27@gmail.com>
2017-08-15 15:54:01 -0500

    added ship locations

--- js/app.js
+++ js/app.js
@@ -20,9 +20,57 @@ var model = {
                   return true;
               }
               else if (index >= 0) {
-                  ship.hits(index)
+                  ship.hits[index] = "hit";
+                  view.displayHit(guess);
+                  view.displayMessage("HIT");
+
+                  if (this.isSunk(ship)) {
+                      view.displayMessage("You sank my battleship!");
+                      this.shipSunk++;
+                  }
+                  return true;
               }
         }
+        view.displayMiss(guess);
+        view.displayMessage("You Missed!");
+        return dalse;
+      },
 
-      }
+      isSunk: function(ship) {
+        for (var i=0; i < this.shipLength; i++){
+            if (ship.hits[i] !== "hit"){
+              return false;
+            }
+        }
+        return true;
+      },
+
+    generateShipLocations: function() {
+        var locations:
+
+        for (var i=0; i < this.numShips; i++) {
+            do {
+              locations = this.generateShip();
+            } while (this.collision(locations));
+            this.ship[i].locations = locations;
+        }
+        console.log("ships array: ");
+        console.log(this.ships);
+    },
+
+  generateShip: function() {
+    var direction = Math.floor(Math.random() * 2);
+    var row, col;
+
+    if(direction === 1) {
+      row = Math.floor(Math.random() * this.boardSize);
+      col = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
+    } else {
+      col = Math.floor(Math.random() * this.boardSize);
+      row = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
+
+    }
+
+  }
+
 }
